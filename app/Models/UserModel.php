6246a2e4b0fd45139ab78eec63c922ae212ea961
Sadiya8924 <sadiyamaritza8924@gmail.com>
2025-03-25 22:54:35 +0700

    Implementasi Authentication

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,21 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; // Implementasi class Authenticatable
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
-    protected $table = 'm_user'; //Mendefiniskan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang dugunakan
-    
-    protected $fillable = ['username', 'nama', 'password', 'level_id']; // Tambahkan atribut yang bisa diisi
+    protected $table = 'm_user'; // Koreksi: $table, bukan Stable
+    protected $primaryKey = 'user_id'; // Koreksi: $primaryKey, bukan SprimaryKey
 
-    public function level():BelongsTo{
-        return $this->belongsTo(LevelModel::class, 'level_id','level_id');
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
+
+    // Relasi ke tabel level
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
-
 }
+
+//<?php
+
+// namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+// class UserModel extends Model
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_user'; //Mendefiniskan nama tabel yang digunakan oleh model ini
+//     protected $primaryKey = 'user_id'; //mendefinisikan primary key dari tabel yang dugunakan
+    
+//     protected $fillable = ['username', 'nama', 'password', 'level_id']; // Tambahkan atribut yang bisa diisi
+
+//     public function level():BelongsTo{
+//         return $this->belongsTo(LevelModel::class, 'level_id','level_id');
+//     }
+
+//}
