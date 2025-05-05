e12564dccbedd6eb82df4edca64a7b118e7bba38
Sadiya8924 <sadiyamaritza8924@gmail.com>
2025-05-05 19:08:39 +0700

    modifikasi

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,57 +2,59 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
-
-class UserModel extends Authenticatable
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
+    }
+
     use HasFactory;
 
-    protected $table = 'm_user'; // mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel yang digunakan oleh model ini
-    protected $fillable = [
-        'level_id',
-        'username',
-        'nama',
-        'foto',
-        'password',
-        'created_at',
-        'updated_at'
-    ];
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
 
-    protected $hidden = ['password']; // Jangan ditampilkan saat select
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image'];
 
-    protected $casts = [ 'password' => 'hashed' ]; // casting password agar otomatis di hash
+    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
 
-    /**
-     * Relasi ke tabel level
-     */
- 
-    public function level(): BelongsTo {
+    public function level(): BelongsTo
+    {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
-
-    /**
-     * Mendapatkan nama role
-     */
-    public function getRoleName(): string {
-        return $this->level->level_name;
+    public function stok()
+    {
+        return $this->hasMany(StokModel::class, 'user_id', 'user_id');
     }
 
-    /**
-     * Cek apakah user memiliki role tertentu
-     */
-    public function hasRole(string $role): bool {
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+    public function hasRole($role): bool
+    {
         return $this->level->level_kode == $role;
     }
-
-    /**
-     * Mendapatkan kode role
-     */
-    public function getRole(){
+    public function getRole(): string
+    {
         return $this->level->level_kode;
     }
 }
