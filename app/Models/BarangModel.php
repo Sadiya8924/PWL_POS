c3ab3948357932c93a913899ad1b6c45835e384d
Sadiya8924 <sadiyamaritza8924@gmail.com>
2025-05-06 16:50:01 +0700

    jobsheet 11

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,12 +10,20 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class BarangModel extends Model
 {
     use HasFactory;
-    protected $table = 'm_barang';
+
+    protected $table = 'm_barang'; 
     protected $primaryKey = 'barang_id';
 
-    protected $fillable = ['kategori_id', 'barang_nama', 'barang_kode', 'harga_beli', 'harga_jual'];
+    protected $fillable = ['barang_nama', 'barang_kode', 'kategori_id', 'harga_beli', 'harga_jual', 'image'];
 
-    public function kategori(): BelongsTo{
+    public function kategori(): BelongsTo
+    {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,9 +22,41 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 use App\Http\Controllers\Api\LevelController;
-
 Route::get('levels', [LevelController::class, 'index']);
 Route::post('levels', [LevelController::class, 'store']);
 Route::get('levels/{level}', [LevelController::class, 'show']);
 Route::put('levels/{level}', [LevelController::class, 'update']);
 Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+use App\Http\Controllers\Api\UserController;
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{user}', [UserController::class, 'show']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
+
+use App\Http\Controllers\Api\KategoriController;
+
+Route::get('kategori', [KategoriController::class, 'index']);
+Route::post('kategori', [KategoriController::class, 'store']);
+Route::get('kategori/{kategori}', [KategoriController::class, 'show']);
+Route::put('kategori/{kategori}', [KategoriController::class, 'update']);
+Route::delete('kategori/{kategori}', [KategoriController::class, 'destroy']);
+
+use App\Http\Controllers\Api\BarangController;
+Route::get('barang', [BarangController::class, 'index']);
+Route::post('barang', [BarangController::class, 'store']);
+Route::get('barang/{barang}', [BarangController::class, 'show']);
+Route::put('barang/{barang}', [BarangController::class, 'update']);
+Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
+
+use App\Http\Controllers\Api\SupplierController;
+Route::get('supplier', [SupplierController::class, 'index']);
+Route::post('supplier', [SupplierController::class, 'store']);
+Route::get('supplier/{supplier}', [SupplierController::class, 'show']);
+Route::put('supplier/{supplier}', [SupplierController::class, 'update']);
+Route::delete('supplier/{supplier}', [SupplierController::class, 'destroy']);
+
+use App\Http\Controllers\Api\PenjualanController;
+Route::post('/penjualan', [PenjualanController::class, 'store']);
+Route::get('/penjualan/{penjualan}', [PenjualanController::class, 'show']);
